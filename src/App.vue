04d911fc151f929f42691055686dbf8dbf4f54d4
ywxt <ywxtcwh@qq.com>
2023-11-27 00:42:04 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import PracticeRadicals from "./components/PracticeRadicals.vue";
+</script>
+
+<template>
+  <el-container class="center-container">
+    <el-header>Header</el-header>
+    <el-main justify="center" align="middle" class="center-content">
+      <PracticeRadicals />
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<style scoped>
+@media (min-width: 768px) {
+  .center-container {
+    width: 750px;
+  }
+}
+
+@media (min-width: 992px) {
+  .center-container {
+    width: 970px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .center-container {
+    width: 1170px;
+  }
+}
+
+.center-container {
+  height: 100vh;
+  /* width: 100vw; */
+  /* 让容器充满整个视口高度 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.center-content {
+  width: 100%;
+  /* 确保内容在容器内居中 */
+}
+</style>
---
+++ src/components/PracticeRadicals.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+type RadicalType = "simplified" | "traditional" | "all";
+const radicalType: Ref<RadicalType> = ref("simplified");
+const tip = ref("");
+const inputValue = ref("");
+const history = ref([]);
+</script>
+
+<template>
+  <!-- 顶部选择框 -->
+  <el-row class="row" justify="end">
+    <el-col :span="3">
+      <el-select v-model="radicalType" placeholder="繁简字根">
+        <el-option label="简体字根" value="simplified"></el-option>
+        <el-option label="繁體字根" value="traditional"></el-option>
+        <el-option label="全部" value="all"></el-option>
+      </el-select>
+    </el-col>
+  </el-row>
+
+  <el-row class="row" justify="center">
+    <el-col :span="5">
+      <el-card>
+        <span class="radical-text">字根</span>
+        <p v-if="tip">{{ tip }}</p>
+      </el-card>
+    </el-col>
+  </el-row>
+  <!-- 中间输入框 -->
+  <el-row class="row" justify="center">
+    <el-col :span="5">
+      <el-input
+        v-model="inputValue"
+        placeholder="请输入内容"
+        size="large"
+      ></el-input>
+    </el-col>
+  </el-row>
+
+  <!-- 底部列表 -->
+  <el-row>
+    <el-col :span="24">
+      <el-table :data="history" style="width: 100%">
+        <el-table-column prop="index" label="序號"></el-table-column>
+        <el-table-column prop="rank" label="成績"></el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
+</template>
+
+<style scoped>
+.row {
+  margin-bottom: 30px;
+}
+.radical-text {
+  font-size: 3rem;
+  font-weight: bold;
+}
+</style>
